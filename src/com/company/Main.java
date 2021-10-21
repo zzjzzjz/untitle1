e0fb6ab8c9b8bc598910fb86ede21c96a27dee41
zzjzzjz <2416971964@qq.com>
2021-10-21 21:54:51 +0800

    comment

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,9 +3,11 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	SpeakElephant speak=new SpeakElephant();
-    speak.start();
-    for(int i=0;i<3000;i++) {
+	SpeakElephant speake=new SpeakElephant();
+    SpeakCat speakc=new SpeakCat();
+    speake.start();
+    speakc.start();
+    for(int i=0;i<300;i++) {
         System.out.println("主线");
     }
     }
